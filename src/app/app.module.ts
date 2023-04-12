e0ffabf45933d50ab8565a5e831f8a1bbb403ca8
rachnakafle <rachanakafle99@gmail.com>
2023-04-12 17:21:09 +0545

    Working on phone number with country code input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,23 @@ import { ConditionalFormComponent } from './conditional-form/conditional-form.co
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReactiveConditionalFormComponent } from './reactive-conditional-form/reactive-conditional-form.component';
 import { ConditionanForm2Component } from './conditionan-form2/conditionan-form2.component';
+import { CountryCodeComponent } from './country-code/country-code.component';
+import {Ng2TelInputModule} from 'ng2-tel-input';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConditionalFormComponent,
     ReactiveConditionalFormComponent,
-    ConditionanForm2Component
+    ConditionanForm2Component,
+    CountryCodeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Ng2TelInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/country-code/country-code.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'country-code',
+  templateUrl: './country-code.component.html',
+  styleUrls: ['./country-code.component.css']
+})
+export class CountryCodeComponent implements OnInit {
+  onlyCountries:any = []
+  constructor() { }
+
+  hasError(e:any){
+
+  }
+
+  getNumber(e:any){
+
+  }
+
+  telInputObject(e:any){
+
+  }
+
+  onCountryChange(e:any){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
